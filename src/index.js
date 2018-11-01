96e8b4b6ee27e0a07dfdd1e3a217972b2cef865e
Unknown <Daniel.quarrell@yahoo.com>
2018-11-01 12:17:13 +0000

    Data and file coded

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+//import Style from ./style.css';
+//import Icon from './icon.png';
+//import Data from './data.xml';
 
 function component() {
 	var element = document.createElement('div');
@@ -9,10 +10,10 @@ function component() {
 	//element.classList.add('hello');
 
 	// Add the image to our existing div.
-	var myIcon = new Image();
-	myIcon.src = Icon;
+	//var myIcon = new Image();
+	//myIcon.src = Icon;
 
-	element.appendChild(myIcon);
+	//element.appendChild(myIcon);
 
 	return element;
 }
